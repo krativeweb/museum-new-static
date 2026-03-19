56f682b76391e98a294441e46e2be0b8f69a2797
krativeweb <krativewebdeveloper@gmail.com>
2026-03-19 13:01:11 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-
-  basePath: "/museum",
-  assetPrefix: "/museum",
-
   images: {
     unoptimized: true,
   },
